e0885b7ba965cd6203455b6b7f856594bd2f41b8
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2023-02-10 18:30:21 +0530

    ui changes

--- lib/screens/dashboard/NavScreen/OrderScreen.dart
+++ lib/screens/dashboard/NavScreen/OrderScreen.dart
@@ -3,14 +3,14 @@ import 'package:adminpannelgrocery/screens/dashboard/dashboard_screen.dart';
 import 'package:adminpannelgrocery/screens/main/components/side_menu.dart';
 import 'package:flutter/material.dart';
 
-class dscreen extends StatefulWidget {
-  const dscreen({Key? key}) : super(key: key);
+class OrderScreen extends StatefulWidget {
+  const OrderScreen({Key? key}) : super(key: key);
 
   @override
-  State<dscreen> createState() => _dscreenState();
+  State<OrderScreen> createState() => OrderScreenState();
 }
 
-class _dscreenState extends State<dscreen> {
+class OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
